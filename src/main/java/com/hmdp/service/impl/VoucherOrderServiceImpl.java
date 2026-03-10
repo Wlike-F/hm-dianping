6ef0a491a9d3e5fab2d6319caae6b87eb6b891dd
wang-ff <wlikef5213@163.com>
2026-03-10 20:50:19 +0800

    优惠劵秒杀

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,20 +1,76 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
  *  服务实现类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    /**
+     * 秒杀优惠券
+     * @param voucherId
+     * @return
+     */
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 从数据库中查询优惠券信息
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        //1、判断优惠券是否在秒杀时间内
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("优惠券尚未开始");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("优惠券已过期");
+        }
+        //2、判断优惠券库存是否充足
+        if (voucher.getStock() < 1) {
+            return Result.fail("优惠券已抢购完!");
+        }
+        //3、下单（扣库存、创建订单）
+        Long userId = UserHolder.getUser().getId();
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if (count > 0) {
+            return Result.fail("用户已抢购过该优惠券");
+        }
+        //4、扣减库存
+        boolean success = seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id", voucherId).update();
+        if (!success) {
+            return Result.fail("库存不足");
+        }
+
+        VoucherOrder order = new VoucherOrder();
+        long order_id = redisIdWorker.nextId("order");
+        order.setId(order_id);
+        order.setUserId(userId);
+        order.setVoucherId(voucherId);
+        order.setCreateTime(LocalDateTime.now());
+        save(order);
+
+        return Result.ok(order_id);
+    }
 }
